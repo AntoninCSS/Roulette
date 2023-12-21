dd9d7fce4a433ae2ad1983dee9606daff6e23709
AntoninC <65115602+AntoninCSS@users.noreply.github.com>
2023-12-21 15:04:59 +0100

    Delete Code directory

--- Code/Modeles/Aleatoire.php
+++
@@ -1,9 +0,0 @@
-<form action="" method="POST">
-    <input name="Tirage" type="submit" class="submit2" value="Tirez !">
-    <input name="Reset" type="submit" class="Reset" value="Reset">
-    <input name="TirageAbsent" type="submit" class="TirageAbsent" value="Tirez un absent !">
-</form>
-
-<?php
-include("../Controleur/Aleatoire_Eleve.php")
-?>
